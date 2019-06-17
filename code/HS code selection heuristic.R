314278a0af6e573c1a74ee1513d57227c78948c6
Johannes <fritz.johannes@gmail.com>
2019-06-17 10:41:58 +0200

    revised approach to classify results

--- code/HS code selection heuristic.R
+++ code/HS code selection heuristic.R
@@ -6,82 +6,256 @@ gta_setwd()
 
 load("17 Shiny/5 HS code finder/database/GTA HS code database.Rdata")
 
-### Goal: Identifying what are the factors best explaining whether a chosen HS code is correct
+phrases.finished=subset(job.phrase, processed==T & job.id %in% subset(job.log, nr.of.checks>1)$job.id)$phrase.id
 
-### 1. I want to look at some descriptive stats to get a feeling: how many users and how many checks do they each process; how many results do each search engine spit out;...
+hs.candidates=as.data.frame(subset(code.suggested, phrase.id %in% phrases.finished & is.na(hs.code.6)==F))
 
-### Checks per user
-table(merge(check.log, users, by="user.id", all.x = T)$name)
+chosen.suggestions=as.data.frame(table(subset(code.selected, check.id %in% subset(check.phrases, phrase.id %in% phrases.finished)$check.id)$suggestion.id))
+names(chosen.suggestions)=c("suggestion.id","nr.times.chosen")
+chosen.suggestions$suggestion.id=as.numeric(as.character(chosen.suggestions$suggestion.id))
 
-### Suggestions per source
-table(merge(code.source, suggestion.sources, by="source.id", all.x = T)$source.name)
-paste("Out of a total suggestions:", length(unique(code.source$suggestion.id)))
+hs.candidates=merge(hs.candidates, chosen.suggestions, by="suggestion.id", all.x=T)
+hs.candidates[is.na(hs.candidates)]=0
+
+checks.per.phrase=as.data.frame(table(subset(check.phrases, phrase.id %in% phrases.finished)$phrase.id))
+names(checks.per.phrase)=c("phrase.id","nr.of.checks")
+checks.per.phrase$phrase.id=as.numeric(as.character(checks.per.phrase$phrase.id))
+
+hs.candidates=merge(hs.candidates, checks.per.phrase, by="phrase.id", all.x=T)
+hs.candidates$selection.share=hs.candidates$nr.times.chosen/hs.candidates$nr.of.checks
+hs.candidates$selection.share[hs.candidates$selection.share>=1]=1
 
 
+### could do a classifier as follows
+## those with full agreement, one way or the other are classified as in or out
+## for those in between, I use a randomforest to see where they should go using the attributes (share of values occuring in chosen cases or as dummies)
+## 1) source, 
+## 2) certainty, 
+## 3) user ID 
+## The KNN would be trained on the clear cases, I guess. 
+## Cannot include the "votes in favor" metric since it's a clear indicator for inclusion ...
+## However, can apply different probability thresholds for inclusion depending on the "votes in favor" metric!
 
-###########################################################################
-### 2. I construct a master dataframe with one row per suggestion
-###########################################################################
-## 2.1 Focus on a dataframe only with the following restrictions:
-jobs.with.at.least.3.checks <- subset(job.log, nr.of.checks>=3)$job.id
-users.with.at.least.10.checks <- subset(aggregate(check.id ~ user.id, check.log, function(x) length(unique(x))), check.id>=10)$user.id
-phrases.with.at.least.3.checks <- subset(aggregate(check.id ~ phrase.id, check.phrases, function(x) length(unique(x))), check.id>=3)$phrase.id
 
-phrases.with.more.than.3.checks <- subset(aggregate(check.id ~ phrase.id, check.phrases, function(x) length(unique(x))), check.id>3)$phrase.id
+## creating the estimation variables
+## sources
+hs.candidates$source.user=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==99)$suggestion.id)
+hs.candidates$source.guru=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==1)$suggestion.id)
+hs.candidates$source.eurostat=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==2)$suggestion.id)
+hs.candidates$source.google=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==3)$suggestion.id)
+hs.candidates$source.zauba=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==4)$suggestion.id)
+hs.candidates$source.ets=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==5)$suggestion.id)
+hs.candidates$source.bianma=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==6)$suggestion.id)
+hs.candidates$source.cybex=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==7)$suggestion.id)
+hs.candidates$source.descr=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==8)$suggestion.id)
+hs.candidates$source.nr=rowSums(hs.candidates[,c(which(grepl("source\\.",names(hs.candidates))))])
 
-## 2.2 Merge dataframes together
-master <- merge(subset(code.suggested, phrase.id %in% phrases.with.at.least.3.checks), phrase.table, all.x = T)
+## check certainty
+certainty.distribution=aggregate(check.id ~ phrase.id + certainty.level,
+                                 merge(check.certainty, subset(check.phrases, phrase.id %in% phrases.finished), by="check.id"), 
+                                 function(x) length(unique(x)))
+certainty.distribution=merge(certainty.distribution, unique(hs.candidates[,c("phrase.id","nr.of.checks")]), by="phrase.id")
+certainty.distribution$certain.share=certainty.distribution$check.id/certainty.distribution$nr.of.checks
 
-# Take out those phrases with no HS code suggested
-master <- subset(master, is.na(hs.code.6)==F)
+certainty.distribution$value=5
+certainty.distribution$value[certainty.distribution$certainty.level=="highly"]=4
+certainty.distribution$value[certainty.distribution$certainty.level=="fairly"]=3
+certainty.distribution$value[certainty.distribution$certainty.level=="somewhat"]=2
+certainty.distribution$value[certainty.distribution$certainty.level=="not"]=1
 
-# Now add 3 check IDs
-test <- aggregate(check.id ~ phrase.id, subset(check.phrases, phrase.id %in% phrases.with.at.least.3.checks), min)
-setnames(test, "check.id", "check1")
-test <- merge(test, aggregate(check.id ~ phrase.id, subset(check.phrases, phrase.id %in% phrases.with.at.least.3.checks), median))
-setnames(test, "check.id", "check2")
-test$check2 <- as.integer(test$check2)
-test <- merge(test, aggregate(check.id ~ phrase.id, subset(check.phrases, phrase.id %in% phrases.with.at.least.3.checks), max))
-setnames(test, "check.id", "check3")
-master <- merge(master, test, all.x = T);rm(test)
+certainty.stats=merge(data.frame(phrase.id=unique(certainty.distribution$phrase.id)),
+                      aggregate(certain.share ~ phrase.id, subset(certainty.distribution, certainty.level %in% c("highly","exactly")), sum),
+                      by="phrase.id", all.x=T)
+certainty.stats$certain.share[certainty.stats$certain.share>1]=1
 
-# Add info on whether phrase was adjusted
-master$adjusted.phrase <- as.numeric(master$check1 %in% words.removed$check.id | master$check2 %in% words.removed$check.id | master$check3 %in% words.removed$check.id)
+certainty.stats=merge(certainty.stats, 
+                      aggregate(value ~phrase.id, certainty.distribution, mean), 
+                      by="phrase.id", all.x=T)
+data.table::setnames(certainty.stats, "value","certainty.mean")
 
-# Add info on whether additional suggestion
-master$additional.suggestion <- as.numeric(master$check1 %in% additional.suggestions$check.id)
+certainty.stats=merge(certainty.stats, 
+                      aggregate(value ~phrase.id, certainty.distribution, median), 
+                      by="phrase.id", all.x=T)
+data.table::setnames(certainty.stats, "value","certainty.median")
 
-# Add user info: 1 if selected, 0 if not, and NA if not checked
-for(user in users.with.at.least.10.checks){
-  eval(parse(text = paste0("master$user.", user, " <- NA ")))
+certainty.stats=merge(certainty.stats, 
+                      aggregate(value ~phrase.id, certainty.distribution, sd), 
+                      by="phrase.id", all.x=T)
+data.table::setnames(certainty.stats, "value","certainty.sd")
+
+certainty.stats[is.na(certainty.stats)]=0
+
+hs.candidates=merge(hs.candidates, certainty.stats, by="phrase.id", all.x=T)
+
+## these are marked as a service
+odd.phrases=subset(hs.candidates, is.na(certain.share))
+hs.candidates=subset(hs.candidates, is.na(certain.share)==F)
+
+### User stats
+for(u.id in unique(check.log$user.id)){
+  u.checks=subset(check.log, user.id==u.id)$check.id
+  user.certainty=merge(subset(check.phrases, check.id %in% u.checks),
+                       subset(check.certainty, check.id %in% u.checks),
+                       by="check.id", all.x=T)
   
-  eval(parse(text = paste0("master$user.", user, "[master$check1 %in% subset(check.log, user.id==", user, ")$check.id] <- 0")))
-  eval(parse(text = paste0("master$user.", user, "[master$check2 %in% subset(check.log, user.id==", user, ")$check.id] <- 0")))
-  eval(parse(text = paste0("master$user.", user, "[master$check3 %in% subset(check.log, user.id==", user, ")$check.id] <- 0")))
-
-  eval(parse(text = paste0("master$user.", user, "[master$check1 %in% subset(check.log, user.id==", user, ")$check.id & master$hs.code.6 %in% subset(code.suggested, suggestion.id %in% subset(code.selected, check.id %in% subset(check.log, user.id==", user, ")$check.id)$suggestion.id)$hs.code.6] <- 1")))
-  eval(parse(text = paste0("master$user.", user, "[master$check2 %in% subset(check.log, user.id==", user, ")$check.id & master$hs.code.6 %in% subset(code.suggested, suggestion.id %in% subset(code.selected, check.id %in% subset(check.log, user.id==", user, ")$check.id)$suggestion.id)$hs.code.6] <- 1")))
-  eval(parse(text = paste0("master$user.", user, "[master$check3 %in% subset(check.log, user.id==", user, ")$check.id & master$hs.code.6 %in% subset(code.suggested, suggestion.id %in% subset(code.selected, check.id %in% subset(check.log, user.id==", user, ")$check.id)$suggestion.id)$hs.code.6] <- 1")))
-  # master$user.1[master$check3 %in% subset(check.log, user.id==1)$check.id & master$hs.code.6 %in% subset(code.suggested, suggestion.id %in% subset(code.selected, check.id %in% subset(check.log, user.id==1)$check.id)$suggestion.id)$hs.code.6] <- 1
-  # subset(code.suggested, suggestion.id %in% subset(code.selected, check.id %in% subset(check.log, user.id==1)$check.id)$suggestion.id)$hs.code.6
+  more.than.once=as.data.frame(table(user.certainty$phrase.id))
+  multi.checks=subset(user.certainty, phrase.id %in% more.than.once$Var1[more.than.once$Freq>1])
+  
+  
+  if(nrow(multi.checks)>0){
+    multi.checks$value=5
+    multi.checks$value[multi.checks$certainty.level=="highly"]=4
+    multi.checks$value[multi.checks$certainty.level=="fairly"]=3
+    multi.checks$value[multi.checks$certainty.level=="somewhat"]=2
+    multi.checks$value[multi.checks$certainty.level=="not"]=1
+    
+    keep.checks=c()
+    for(p.id in unique(multi.checks$phrase.id)){
+      max.cl=max(multi.checks$value[multi.checks$phrase.id==p.id])
+      keep.checks=c(keep.checks, min(multi.checks$check.id[multi.checks$phrase.id==p.id & multi.checks$value==max.cl]))
+      
+    }
+    
+    user.certainty=rbind(subset(user.certainty, phrase.id %in% more.than.once$Var1[more.than.once$Freq==1]),
+                         subset(user.certainty, check.id %in% keep.checks))
+  }
+  
+  user.certainty=unique(subset(user.certainty, is.na(certainty.level)==F))
+  
+  ## second iteration for duplicated check.ids
+  more.than.once=as.data.frame(table(user.certainty$phrase.id))
+  multi.checks=subset(user.certainty, phrase.id %in% more.than.once$Var1[more.than.once$Freq>1])
+  
+  if(nrow(multi.checks)>0){
+    multi.checks$value=5
+    multi.checks$value[multi.checks$certainty.level=="highly"]=4
+    multi.checks$value[multi.checks$certainty.level=="fairly"]=3
+    multi.checks$value[multi.checks$certainty.level=="somewhat"]=2
+    multi.checks$value[multi.checks$certainty.level=="not"]=1
+    
+    for(p.id in unique(multi.checks$phrase.id)){
+      min.cl=min(multi.checks$value[multi.checks$phrase.id==p.id])
+      user.certainty$certainty.level[user.certainty$phrase.id==p.id]=multi.checks$certainty.level[multi.checks$phrase.id==p.id & multi.checks$value==min.cl]
+      
+    }
+    
+  }
+  
+  
+  user.certainty=unique(subset(user.certainty, is.na(certainty.level)==F))
+  user.certainty$certainty.level=as.character(user.certainty$certainty.level)
+  
+  hs.candidates=merge(hs.candidates, unique(user.certainty[,c("phrase.id","certainty.level")]), by="phrase.id",all.x=T)
+  
+  user.selection=unique(subset(code.selected, check.id %in% user.certainty$check.id)$suggestion.id)
+  user.phrases=unique(user.certainty$phrase.id)
+  hs.candidates$certainty.level[hs.candidates$suggestion.id %in% user.selection]=paste("selected-",hs.candidates$certainty.level[hs.candidates$suggestion.id %in% user.selection], sep="")
+  hs.candidates$certainty.level[!hs.candidates$suggestion.id %in% user.selection & hs.candidates$phrase.id %in% user.phrases]=paste("discarded-",hs.candidates$certainty.level[!hs.candidates$suggestion.id %in% user.selection & hs.candidates$phrase.id %in% user.phrases], sep="")
+  hs.candidates$certainty.level[is.na(hs.candidates$certainty.level)]="not checked"
+  hs.candidates$certainty.level=as.factor(hs.candidates$certainty.level)
+  data.table::setnames(hs.candidates, "certainty.level",paste("user.", u.id, sep=""))
+  
 }
 
-test <- aggregate()
+## shared CPC code with highly agreeable/disagreeabe selections
+agreeable.threshold=2/3
+disagreeable.threshold=1/3
 
-master$nr.user <- sum(master$user.1 + master$user.2 + master$user.8 + master$user.15)
+phrase.cpc=unique(hs.candidates[,c("phrase.id","hs.code.6", "selection.share")])
+phrase.cpc$hs=as.numeric(as.character(phrase.cpc$hs.code.6))
+phrase.cpc=merge(phrase.cpc, cpc.to.hs, by="hs")
+cpc.per.phrase=aggregate(cpc ~phrase.id, phrase.cpc, function(x) length(unique(x)))
+
+phrase.id.vector=phrase.cpc$phrase.id
+cpc.vector=phrase.cpc$cpc
+selection.vector=phrase.cpc$selection.share
+
+agreeable.positions=which(selection.vector>=agreeable.threshold)
+disagreeable.positions=which(selection.vector<=disagreeable.threshold)
+
+agree.vector=character(nrow(phrase.cpc))
+disagree.vector=character(nrow(phrase.cpc))
+
+
+for(i in 1:nrow(phrase.cpc)){
+  phrase.positions=which(phrase.id.vector==phrase.id.vector[i])
+  agree.vector[i]=cpc.vector[i] %in% cpc.vector[setdiff(intersect(phrase.positions, agreeable.positions),i)]
+  disagree.vector[i]=cpc.vector[i] %in% cpc.vector[setdiff(intersect(phrase.positions, disagreeable.positions),i)]
+  
+  print(i/nrow(phrase.cpc))
+}
+
+phrase.cpc$cpc.agree=agree.vector
+phrase.cpc$cpc.disagree=disagree.vector
+
+phrase.cpc$share.cpc="neither"
+phrase.cpc$share.cpc[phrase.cpc$cpc.agree==T & phrase.cpc$cpc.disagree==T]="both"
+phrase.cpc$share.cpc[phrase.cpc$cpc.agree==T & phrase.cpc$cpc.disagree==F]="agree"
+phrase.cpc$share.cpc[phrase.cpc$cpc.agree==F & phrase.cpc$cpc.disagree==T]="disagree"
+phrase.cpc$share.cpc=as.factor(phrase.cpc$share.cpc)
+
+hs.candidates=merge(hs.candidates, phrase.cpc[,c("phrase.id","hs.code.6","share.cpc")], by=c("phrase.id","hs.code.6"))
+
+## some cleaning
+## user 11 and 23 have only one result
+hs.candidates$user.11=NULL
+hs.candidates$user.23=NULL
+
+
+#### ESTIMATION
+## splitting off the training set from the estimation set
+hs.out.threshold=.10
+hs.in.threshold=.9
+train.share=.7
+
+estimation.set=subset(hs.candidates, selection.share>hs.out.threshold & selection.share<hs.in.threshold)
+
+training.set=subset(hs.candidates, (selection.share<=hs.out.threshold)|(selection.share>=hs.in.threshold))
+training.set$evaluation=as.numeric(training.set$selection.share>.5)
+training.set$train.id=1:nrow(training.set)
+
+## preparing the training set
+train.split=sample(unique(training.set$train.id), ceiling(nrow(training.set)*train.share))
+
+train=training.set[,setdiff(names(training.set), c("phrase.id","suggestion.id","hs.code.6","nr.times.chosen","nr.of.checks","selection.share"))]
+
+train.x = subset(train, train.id %in% train.split)
+test.x = subset(train, ! train.id %in% train.split)
+
+test.y=test.x$evaluation
+test.x$train.id=NULL
+test.x$evaluation=NULL
+
+train.y=train.x$evaluation
+train.x$train.id=NULL
+train.x$evaluation=NULL
+
+library(SuperLearner)
+hs.classifier = SuperLearner(train.y,train.x, family = binomial(),
+                                  SL.library = "SL.xgboost")
+
+pred.train=data.frame(obs=train.y, pred=predict(hs.classifier, train.x)$pred[,1])
+pred.test= data.frame(obs=test.y, pred=predict(hs.classifier, test.x)$pred[,1])
+
+
+pred_rocr = ROCR::prediction(pred.train$pred, pred.train$obs)
+train.auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+train.auc
+
+pred_rocr = ROCR::prediction(pred.test$pred, pred.test$obs)
+test.auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+test.auc
+
+save(hs.classifier, test.auc, train.auc, file="17 Shiny/5 HS code finder/database/HS classifier.Rdata")
+
+## estimating the unsure cases
+estimate=estimation.set[,setdiff(names(estimation.set), c("phrase.id","suggestion.id","hs.code.6","nr.times.chosen","nr.of.checks","selection.share"))]
 
 
 
+estimate$train.id=NULL
+estimate$evaluation=NULL
 
-##
+estimation.set$relevance= predict(hs.classifier, estimate)$pred[,1]
 
-
-###########################################################################
-### 3. I identify what I consider to be a correct match.
-###########################################################################
-## 3.1 Approach: Where all three users agree
-
-## 3.2 Approach: At least 2 users agree and have certainty.level equal to 1 or 2
-
-
-
+save(estimation.set, training.set, test.auc, train.auc, users, file="17 Shiny/5 HS code finder/database/HS classifier data.Rdata")
