fc53ae5431c84effc78d9a10e14b630dc21f1572
Johannes <fritz.johannes@gmail.com>
2019-10-25 15:59:03 +0200

    loading job.log and user.list seems unnecessary

--- code/functions/gta_hs_process_completed_phrase.R
+++ code/functions/gta_hs_process_completed_phrase.R
@@ -11,13 +11,6 @@ gta_hs_process_completed_phrase <- function(processed.phrase=NULL, path = NULL){
     stop("Please specify a path to the database.")
   }
   
-  # load_all(path)
-  job.log <- change_encoding(gta_sql_load_table("job_log"))
-  job.log <<- job.log
-  users <- change_encoding(gta_sql_load_table("user_log",table.prefix = "gta_"))
-  users <<- users
-  
-
   classifier.variables=gta_hs_create_classifier_variables_phrase(phrase.ids=processed.phrase)
   classifier.variables<<-classifier.variables
   classification.result=gta_hs_classify_results("classifier.variables")
