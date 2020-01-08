d81ab4d8bd51ab018b4d784a7ad5bfb3a257b76f
Johannes Fritz <fritz.johannes@gmail.com>
2020-01-08 16:00:04 +0100

    bug bug bug fix fix fix

---
+++ code/functions/gta_hs_classify_results.R
@@ -0,0 +1,194 @@
+gta_hs_classify_results<- function(processed.phrase=NULL,
+                                   job.id=NULL,
+                                   relevance.threshold=.5,
+                                   path.to.cloud=NULL,
+                                   source.data="17 Shiny/5 HS code finder/database/HS classifier.Rdata"){
+  
+
+  # variable.df="classifier.variables"
+
+  # relevance.threshold=.5
+  # path.to.cloud=NULL
+  # source.data="17 Shiny/5 HS code finder/database/HS classifier.Rdata"
+  # processed.phrase = "classifier.variables"
+  # job.id=NULL
+  
+  library(gtalibrary)
+  library(gtasql)
+  library(pool)
+  library(data.table)
+  
+  setwd("/home/rstudio/Dropbox/GTA cloud")
+  # gta_setwd()
+  
+  database = "ricardomain"
+  gta_sql_pool_open(db.title=database,
+                   db.host = gta_pwd(database)$host,
+                   db.name = gta_pwd(database)$name,
+                   db.user = gta_pwd(database)$user,
+                   db.password = gta_pwd(database)$password,
+                   table.prefix = "hs_")
+
+
+  if(is.null(processed.phrase)){
+    stop("gta_hs_classify_results: No ID for the processed phrase is specified.")
+  }
+  
+  estimation.set=gta_hs_create_classifier_variables_phrase(phrase.ids=processed.phrase)
+  
+  agreed.parts=subset(estimation.set, selection.share %in% c(0,1))
+  agreed.parts$probability=agreed.parts$selection.share
+  agreed.parts$relevant=agreed.parts$selection.share
+  
+  estimation.set=subset(estimation.set, ! selection.share %in% c(0,1))
+  
+  if(is.null(path.to.cloud)==F){
+    setwd(path.to.cloud)
+  } else {
+    if(grepl(" cloud", getwd())){
+      gta_setwd()
+    } else{
+      stop("You are not in the GTA cloud folder, please specify 'path.to.cloud' or use setwd() yourself.")
+      gta_sql_pool_close()
+    }
+  }
+  
+  ## estimating disagreements, if there are any
+  if(nrow(estimation.set)>0){
+    
+    load(source.data)
+    
+    ## A variable i smissing
+    if(any(! classifier.variables %in% names(estimation.set))){
+      
+      miss.vars=paste(classifier.variables[! classifier.variables %in% names(estimation.set)], collapse=";")
+      
+      # SEND EMAIL TO JF
+      sender = gta_pwd("mail")$mail  
+      recipients = c("fritz.johannes@gmail.com","patrick.buess@student.unisg.ch")
+      attachment=NULL
+      
+      sbjct=paste("[HS App] URGENT: Classifier misses variable", sep="")
+      message=paste0("Hello \n\n The HS App classifier needs re-estimation.\n\nThe missing variable(s): ",miss.vars,"\n\nRegards\nHS app\n\n\n\n", sep="")
+      send.mail(from = sender,
+                to = recipients,
+                subject=sbjct,
+                body=message,
+                html=F,
+                attach.files = attachment,
+                smtp = list(host.name = gta_pwd("mail")$host,
+                            port=gta_pwd("mail")$port,
+                            user.name=sender, 
+                            passwd=gta_pwd("mail")$password,
+                            tls=T),
+                authenticate = T)
+      
+      rm(recipients, message, sbjct, sender)
+      
+      stop("FATAL ERROR: Variable MISSING for classifier.")
+      
+      
+    } 
+    
+    ## there is a new variable
+    estimation.vars=c(classifier.variables,"phrase.id","hs.code.6","suggestion.id","nr.times.chosen","nr.of.checks","selection.share")
+    if(any(! names(estimation.set) %in% estimation.vars)){
+      
+      new.vars=paste(names(estimation.set)[! names(estimation.set) %in% estimation.vars], collapse=";")
+      
+      # SEND EMAIL TO JF
+      sender = gta_pwd("mail")$mail  
+      recipients = c("fritz.johannes@gmail.com","patrick.buess@student.unisg.ch")
+      attachment=NULL
+      
+      sbjct=paste("[HS App] Classifier needs re-estimation", sep="")
+      message=paste0("Hello \n\n The HS App classifier needs re-estimation.\n\nThe new variable(s): ",new.vars,"\n\nWhen you find the time, someone please run 'gta_hs_estimate_classifier()' on his local machine (not a server).\n\nRegards\nHS app\n\n\n\n", sep="")
+      send.mail(from = sender,
+                to = recipients,
+                subject=sbjct,
+                body=message,
+                html=F,
+                attach.files = attachment,
+                smtp = list(host.name = gta_pwd("mail")$host,
+                            port=gta_pwd("mail")$port,
+                            user.name=sender, 
+                            passwd=gta_pwd("mail")$password,
+                            tls=T),
+                authenticate = T)
+      
+      rm(recipients, message, sbjct, sender)
+      
+      
+    } 
+    
+    
+    estimate=estimation.set[,classifier.variables]
+    
+    
+    ## estimating the unsure cases
+    estimation.set$probability=round(predict(hs.classifier, estimate)$pred[,1],3)
+    estimation.set$relevant=as.numeric(estimation.set$probability>=relevance.threshold)
+    
+    ## adding estimates & agreed cases
+    estimation.set=rbind(estimation.set, agreed.parts)
+    
+    
+  } else {
+    estimation.set=agreed.parts
+  }
+  
+  ##  Updating database for processed suggestions
+  for (suggestion in unique(estimation.set$suggestion.id)){
+    sql <- paste0("UPDATE hs_code_suggested 
+                  SET probability = ", 
+                  min(estimation.set$probability[estimation.set$suggestion.id==suggestion]), "
+                  WHERE suggestion_id = ", suggestion,";")
+    query <- sqlInterpolate(pool,
+                            sql)
+    gta_sql_update_table(query)
+    rm(query, sql)
+    
+    
+  }
+  
+  # GET MAX PROBABILITY AND DECIDE WHETHER PRHASE IS PROCESSED OR NOT
+  for(this.phrase in unique(estimation.set$phrase.id)){
+    
+    max.prob=max(estimation.set$probability[estimation.set$phrase.id==this.phrase], na.rm = T)
+    
+    if (max.prob > relevance.threshold) {
+      
+      sql <- "UPDATE hs_job_phrase SET processed = 1 WHERE (phrase_id = ?phraseID AND job_id = ?jobID);"
+      query <- sqlInterpolate(pool,
+                              sql,
+                              phraseID = this.phrase,
+                              jobID = job.id)
+      gta_sql_update_table(query)
+      
+      sql <- "UPDATE hs_phrase_log SET exit_status = 2 WHERE phrase_id = ?phraseID;"
+      query <- sqlInterpolate(pool,
+                              sql,
+                              phraseID = this.phrase)
+      gta_sql_update_table(query)
+      
+    } else {
+      
+      sql <- "UPDATE hs_phrase_log SET processing_round = processing_round + 1 WHERE phrase_id = ?phraseID;"
+      query <- sqlInterpolate(pool,
+                              sql,
+                              phraseID = this.phrase)
+      gta_sql_update_table(query)
+      
+      sql <- "UPDATE hs_job_phrase SET processed = 0 WHERE (phrase_id = ?phraseID AND job_id = ?jobID);"
+      query <- sqlInterpolate(pool,
+                              sql,
+                              phraseID = this.phrase,
+                              jobID = job.id)
+      gta_sql_update_table(query)
+      
+    }
+    
+  }
+  print("Fully classified!")
+  gta_sql_pool_close()
+}
---
+++ code/functions/gta_hs_create_classifier_variables_phrase.R
@@ -0,0 +1,295 @@
+gta_hs_create_classifier_variables_phrase<- function(phrase.ids=NULL,
+                                                     job.checks=3,
+                                                     agreeable.threshold=.2,
+                                                     path.to.cloud=NULL){
+  
+  library(gtalibrary)
+  
+  if(is.null(path.to.cloud)==F){
+    setwd(path.to.cloud)
+  } else {
+    if(grepl(" cloud", getwd())){
+      gta_setwd()
+    } else{
+      stop("You are not in the GTA cloud folder, please specify 'path.to.cloud' or use setwd() yourself.")
+    }
+  }
+  
+  
+  job.phrase <- gta_sql_load_table("job_phrase")
+  job.phrase <<- job.phrase
+  job.log <- gta_sql_load_table("job_log")
+  job.log <<- job.log
+  code.suggested <- gta_sql_load_table("code_suggested")
+  code.suggested <<- code.suggested
+  check.phrases <- gta_sql_load_table("check_phrases")
+  check.phrases <<- check.phrases
+  check.certainty <- gta_sql_load_table("check_certainty")
+  check.certainty <<- check.certainty
+  levels.of.certainty <- gta_sql_load_table("levels_of_certainty")
+  levels.of.certainty <<- levels.of.certainty
+  check.log <- gta_sql_load_table("check_log")
+  check.log <<- check.log
+  code.selected <- gta_sql_load_table("code_selected")
+  code.selected <<- code.selected
+  code.source <- gta_sql_load_table("code.source")
+  code.source <<- code.source
+  
+  
+  
+  # if(any(! phrase.ids %in% subset(job.phrase, processed==T)$phrase.id)){
+  #   stop("Some of the phrases you inserted are not processed.")
+  # }
+  
+  hs.candidates=as.data.frame(subset(code.suggested, phrase.id %in% phrase.ids & is.na(hs.code.6)==F))
+  hs.candidates$probability=NULL
+  
+  chosen.suggestions=unique(subset(code.selected, check.id %in% subset(check.phrases, phrase.id %in% phrase.ids)$check.id))
+  
+  chosen.suggestions=merge(chosen.suggestions,
+                           unique(code.suggested[,c("phrase.id","suggestion.id","hs.code.6")]),
+                           by="suggestion.id", all.x=T)
+  chosen.suggestions=aggregate(check.id ~hs.code.6 + phrase.id, chosen.suggestions, function(x) length(unique(x)))
+  names(chosen.suggestions)=c("hs.code.6","phrase.id","nr.times.chosen")
+  
+  hs.candidates=merge(hs.candidates, chosen.suggestions, by=c("phrase.id","hs.code.6"), all.x=T)
+  hs.candidates[is.na(hs.candidates)]=0
+  
+  
+  checks.per.phrase=aggregate(check.id ~ phrase.id, subset(check.phrases, phrase.id %in% phrase.ids & check.id %in% code.selected$check.id), function(x) length(unique(x)))
+  names(checks.per.phrase)=c("phrase.id","nr.of.checks")
+  checks.per.phrase$phrase.id=as.numeric(as.character(checks.per.phrase$phrase.id))
+  
+  hs.candidates=merge(hs.candidates, checks.per.phrase, by="phrase.id", all.x=T)
+  hs.candidates$selection.share=hs.candidates$nr.times.chosen/hs.candidates$nr.of.checks
+  hs.candidates$selection.share[hs.candidates$selection.share>=1]=1
+  
+  
+  hs.mx=aggregate(nr.times.chosen ~phrase.id, hs.candidates, max)
+  setnames(hs.mx, "nr.times.chosen","is.top.choice")
+  
+  hs.candidates=merge(hs.candidates,
+                      hs.mx,
+                      by="phrase.id", all.x=T)
+  
+  hs.candidates$is.top.choice=hs.candidates$is.top.choice==hs.candidates$nr.times.chosen
+  hs.candidates$is.top.choice[is.na(hs.candidates$is.top.choice)]=FALSE
+  
+  
+  rm(hs.mx)
+  
+  ### could do a classifier as follows
+  ## those with full agreement, one way or the other are classified as in or out
+  ## for those in between, I use a randomforest to see where they should go using the attributes (share of values occuring in chosen cases or as dummies)
+  ## 1) source, 
+  ## 2) certainty, 
+  ## 3) user ID 
+  ## The KNN would be trained on the clear cases, I guess. 
+  ## Cannot include the "votes in favor" metric since it's a clear indicator for inclusion ...
+  ## However, can apply different probability thresholds for inclusion depending on the "votes in favor" metric!
+  
+  
+  ## creating the estimation variables
+  ## sources
+  hs.candidates$source.user=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==99)$suggestion.id)
+  hs.candidates$source.guru=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==1)$suggestion.id)
+  hs.candidates$source.eurostat=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==2)$suggestion.id)
+  hs.candidates$source.google=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==3)$suggestion.id)
+  hs.candidates$source.zauba=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==4)$suggestion.id)
+  hs.candidates$source.ets=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==5)$suggestion.id)
+  hs.candidates$source.bianma=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==6)$suggestion.id)
+  hs.candidates$source.cybex=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==7)$suggestion.id)
+  hs.candidates$source.descr=as.numeric(hs.candidates$suggestion.id %in% subset(code.source, source.id==8)$suggestion.id)
+  hs.candidates$source.nr=rowSums(hs.candidates[,c(which(grepl("source\\.",names(hs.candidates))))])
+  
+  ## check certainty
+  certainty.distribution=aggregate(check.id ~ phrase.id + certainty.level,
+                                   merge(check.certainty, subset(check.phrases, phrase.id %in% phrase.ids), by="check.id"), 
+                                   function(x) length(unique(x)))
+  certainty.distribution=merge(certainty.distribution, unique(hs.candidates[,c("phrase.id","nr.of.checks")]), by="phrase.id")
+  certainty.distribution$certain.share=certainty.distribution$check.id/certainty.distribution$nr.of.checks
+  
+  certainty.distribution$value=5
+  certainty.distribution$value[certainty.distribution$certainty.level=="highly"]=4
+  certainty.distribution$value[certainty.distribution$certainty.level=="fairly"]=3
+  certainty.distribution$value[certainty.distribution$certainty.level=="somewhat"]=2
+  certainty.distribution$value[certainty.distribution$certainty.level=="not"]=1
+  
+  if(nrow(subset(certainty.distribution, certainty.level %in% c("highly","exactly")))>0){
+    
+    certainty.share=aggregate(certain.share ~ phrase.id, subset(certainty.distribution, certainty.level %in% c("highly","exactly")), sum)
+    
+  } else {
+    
+    certainty.share=data.frame(phrase.id=phrase.ids,
+                               certain.share=0,
+                               stringsAsFactors = F)
+  }
+  
+  
+  if(any(!phrase.ids %in% certainty.share$phrase.id)){
+    
+    certainty.share=rbind(certainty.share,
+                          data.frame(phrase.id=phrase.ids[!phrase.ids %in% certainty.share$phrase.id],
+                                     certain.share=0,
+                                     stringsAsFactors = F))
+    
+  }
+  
+  certainty.stats=merge(data.frame(phrase.id=unique(certainty.distribution$phrase.id)),
+                        certainty.share,
+                        by="phrase.id", all.x=T)
+  certainty.stats$certain.share[certainty.stats$certain.share>1]=1
+  
+  certainty.stats=merge(certainty.stats, 
+                        aggregate(value ~phrase.id, certainty.distribution, mean), 
+                        by="phrase.id", all.x=T)
+  data.table::setnames(certainty.stats, "value","certainty.mean")
+  
+  certainty.stats=merge(certainty.stats, 
+                        aggregate(value ~phrase.id, certainty.distribution, median), 
+                        by="phrase.id", all.x=T)
+  data.table::setnames(certainty.stats, "value","certainty.median")
+  
+  certainty.stats=merge(certainty.stats, 
+                        aggregate(value ~phrase.id, certainty.distribution, sd), 
+                        by="phrase.id", all.x=T)
+  data.table::setnames(certainty.stats, "value","certainty.sd")
+  
+  certainty.stats[is.na(certainty.stats)]=0
+  
+  hs.candidates=merge(hs.candidates, certainty.stats, by="phrase.id", all.x=T)
+  
+  ## these are marked as a service
+  odd.phrases=subset(hs.candidates, is.na(certain.share))
+  hs.candidates=subset(hs.candidates, is.na(certain.share)==F)
+  
+  ### User stats
+  
+  app.users=aggregate(check.id ~ user.id, check.log, function(x) length(unique(x)))
+  app.users=app.users$user.id[app.users$check.id>=50]
+  
+  choice.factors=c("Not checked","Discarded","Selected",
+                   paste("Discarded",unique(levels.of.certainty$certainty.name), sep="-"),
+                   paste("Selected",unique(levels.of.certainty$certainty.name), sep="-"))
+  
+  base.phrase=subset(code.suggested, phrase.id %in% phrase.ids)[,c("phrase.id","suggestion.id")]
+  
+  phrase.checks=unique(subset(check.phrases, phrase.id %in% phrase.ids)$check.id)
+  
+  for(u.id in app.users){
+    
+    user.checks=unique(subset(check.log, user.id==u.id)$check.id)
+    
+    if(any(user.checks %in% phrase.checks)){
+
+      
+      user.phrases=unique(subset(check.phrases, phrase.id %in% phrase.ids & check.id %in% user.checks)$phrase.id)
+      
+      # only taking most recent in case of multiple checks per phrase
+      user.checks=aggregate(check.id ~phrase.id, subset(check.phrases, check.id %in% user.checks & phrase.id %in% user.phrases), max)$check.id
+      
+      u.check=subset(check.phrases, check.id %in% user.checks)
+      u.check=merge(u.check, code.suggested[,c("phrase.id","suggestion.id")], by="phrase.id", all.x=T)
+      
+      u.select=subset(code.selected, check.id %in% user.checks)
+      if(nrow(u.select)>0){
+       
+        u.select$user.choice="Selected"
+        
+        u.check=merge(u.check, u.select,by=c("check.id","suggestion.id"), all.x=T)
+         
+      } else {
+        u.check$user.choice="Discarded"
+      }
+      rm(u.select)
+      
+      
+      u.check$user.choice[is.na(u.check$user.choice)]="Discarded"
+      
+      for(level in unique(check.certainty$certainty.level)){
+        level.checks=subset(check.certainty, certainty.level==level)$check.id
+        u.check$user.choice[u.check$check.id %in% level.checks]=paste(u.check$user.choice[u.check$check.id %in% level.checks],level, sep="-")
+        
+      }
+      u.check$check.id=NULL
+      
+      u.check=merge(base.phrase, u.check, by=c("phrase.id", "suggestion.id"), all.x=T)
+      u.check$user.choice=factor(u.check$user.choice, levels=choice.factors)
+      
+      ##  correcting for multiple checks in different processing rounds.
+      mx.rnd=aggregate(processing.round ~phrase.id, u.check, max)
+      u.check=merge(u.check, mx.rnd, by=c("phrase.id","processing.round"))
+      
+      u.check=unique(u.check)
+      u.check$processing.round=NULL
+      
+            
+    } else {
+      
+      u.check=base.phrase
+      u.check$user.choice="Not checked"
+      u.check$user.choice=factor(u.check$user.choice, levels=choice.factors)
+      
+
+    }
+    
+    if(any(is.na(u.check$user.choice))){
+      
+      u.check$user.choice[is.na(u.check$user.choice)]="Not checked"
+      u.check$user.choice=factor(u.check$user.choice, levels=choice.factors)
+      
+    }
+    
+    setnames(u.check, "user.choice", paste0("user.",u.id))
+    
+    hs.candidates=merge(hs.candidates, u.check, by=c("phrase.id","suggestion.id"), all.x=T)
+    hs.candidates[is.na(hs.candidates)]="Not checked"
+    
+    rm(u.check)
+    print(u.id)
+    
+  }
+  
+  
+  ## shared CPC code with common acceptance/refusal selections
+  phrase.cpc=unique(hs.candidates[,c("phrase.id","hs.code.6","selection.share")])
+  phrase.cpc$hs=as.numeric(as.character(phrase.cpc$hs.code.6))
+  phrase.cpc=merge(phrase.cpc, cpc.to.hs, by="hs")
+  
+  agreed=unique(subset(phrase.cpc, selection.share>=(1-agreeable.threshold))[,c("phrase.id","cpc")])
+  if(nrow(agreed)>0){
+    agreed$cpc.chosen=T
+    phrase.cpc=merge(phrase.cpc, agreed, by =c("phrase.id","cpc"), all.x=T)
+    phrase.cpc$cpc.chosen[is.na(phrase.cpc$cpc.chosen)]=F
+    
+  } else {
+    phrase.cpc$cpc.chosen=F
+  }
+  
+  
+  refused=unique(subset(phrase.cpc, selection.share<=(agreeable.threshold))[,c("phrase.id","cpc")])
+  if(nrow(refused)>0){
+    
+    refused$cpc.refused=T
+    phrase.cpc=merge(phrase.cpc, refused, by =c("phrase.id","cpc"), all.x=T)
+    phrase.cpc$cpc.refused[is.na(phrase.cpc$cpc.refused)]=F
+    
+  } else {
+    phrase.cpc$cpc.refused=F
+  }
+  
+  
+  
+  
+  agreement.levels=c("neither","both","chosen","refused")
+  phrase.cpc$share.cpc="neither"
+  phrase.cpc$share.cpc[phrase.cpc$cpc.chosen==T & phrase.cpc$cpc.refused==T]="both"
+  phrase.cpc$share.cpc[phrase.cpc$cpc.chosen==T & phrase.cpc$cpc.refused==F]="chosen"
+  phrase.cpc$share.cpc[phrase.cpc$cpc.chosen==F & phrase.cpc$cpc.refused==T]="refused"
+  phrase.cpc$share.cpc=factor(phrase.cpc$share.cpc, levels = agreement.levels)
+  
+  hs.candidates=merge(hs.candidates, phrase.cpc[,c("phrase.id","hs.code.6","share.cpc")], by=c("phrase.id","hs.code.6"))
+  
+  return(hs.candidates)
+}
